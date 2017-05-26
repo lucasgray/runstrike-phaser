0badc589001340aba75e4fcd602cd8b22fcfe34e
Jerome Russ <jerome.d.russ+github@gmail.com>
2017-05-26 13:40:32 -0500

    switched grid setup to Mission object since it should be universal

--- src/js/missions/Mission.js
+++ src/js/missions/Mission.js
@@ -1,12 +1,49 @@
+import * as gameObjects from "../objects";
 export default class Mission {
     constructor(game) {
       this.game = game;
     }
 
-    update(objects){
+    update(){
     }
 
     shutdown(){
       this.game.stage.backgroundColor = this.previousBackground;
+    }
+
+    create(){
+      this.createGrid();
+      this.game.inputHandler = ()=>'';
+    }
+
+    createGrid(){
+      this.game.enemies = this.game.add.physicsGroup();
+      this.game.add.sprite(0,0,'grid');
+      this.cellWidth = (this.game.world.width * .8) / this.gridSize.x;
+      this.cellHeight = (this.game.world.height * .8) / this.gridSize.y;
+      this.setupGrid();
+    }
+
+    setupGrid() {
+        let easystar = new EasyStar.js();
+
+        var grid = [];
+        Array.from(new Array(this.gridSize.y)).forEach(() => {
+            grid.push(new Array(this.gridSize.x).fill(0));
+        });
+
+        this.game.gameData.placedItems.forEach((it) => {
+          grid[it.y][it.x] = 1;
+          new gameObjects[it.type](this.game, it.x * this.cellWidth, it.y * this.cellHeight);
+        });
+
+        easystar.setGrid(grid);
+        easystar.setAcceptableTiles([0]);
+        easystar.calculate();
+        easystar.enableDiagonals();
+        easystar.disableCornerCutting();
+
+        this.game.easystar = easystar;
+        this.game.easystar.calculate();
     }
 }
--- src/js/states/play.js
+++ src/js/states/play.js
@@ -8,16 +8,12 @@ import * as inputHandlers from "../handlers";
 export default class Play extends Phaser.State {
 
     preload() {
-        this.game.create.grid('grid', this.game.width, this.game.height, 64, 64, '#ffffff');
         this.graphics = this.game.add.graphics(0, 0);
-        this.mission = new missions[this.game.mission](this.game);
-        //TODO: look through all missions and add the sprites used!
-        this.mission.enemies.forEach((it) => {
-          console.log(it.image);
+
+        //Loop through the mission's sprite and preload them if they aren't already there
+        this.game.mission.enemies.forEach((it) => {
           if(it.image){
-            console.log('there is an image!');
             if(!this.game.cache.checkImageKey(it.image)){
-              console.log('...and it wasnt cached yet!');
               this.game.load.image(it.image, it.imageSrc, it.imageSize.x, it.imageSize.y);
             }
           }
@@ -25,51 +21,12 @@ export default class Play extends Phaser.State {
     }
 
     create() {
-        this.game.add.sprite(0,0,'grid');
-        this.cellWidth = this.game.world.width / 10;
-        this.cellHeight = this.game.world.height / 15;
-
-        this.game.inputHandler = ()=>'';
-
-        this.game.enemies = this.game.add.physicsGroup();
-
-        this.setupGrid();
+        this.game.mission.create();
 
         this.drawHealth();
 
         this.drawInput();
 
-    }
-
-    setupGrid() {
-
-        //10x15 grid
-        let x = 10;
-        let y = 15;
-
-        //10x15 grid to make it easy.
-
-        let easystar = new EasyStar.js();
-
-        var grid = [];
-
-        Array.from(new Array(y)).forEach(() => {
-            grid.push(new Array(x).fill(0));
-        });
-
-        this.game.gameData.placedItems.forEach((it) => {
-          grid[it.y][it.x] = 1;
-          new gameObjects[it.type](this.game, it.x * this.cellWidth, it.y * this.cellHeight);
-        });
-
-        easystar.setGrid(grid);
-        easystar.setAcceptableTiles([0]);
-        easystar.calculate();
-        easystar.enableDiagonals();
-        easystar.disableCornerCutting();
-
-        this.game.easystar = easystar;
-        this.game.easystar.calculate();
     }
 
     drawHealth() {
@@ -106,12 +63,12 @@ export default class Play extends Phaser.State {
     }
 
     update() {
-        this.mission.update();
+        this.game.mission.update();
         this.game.easystar.calculate();
     }
 
     shutdown() {
         console.log("shut down called");
-        this.mission.shutdown();
+        this.game.mission.shutdown();
     }
 }
