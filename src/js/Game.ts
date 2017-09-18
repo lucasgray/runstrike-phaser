70238faa1b836b09aaaf999f1ae302cacf5d64ac
lucas <lucas.e.gray@gmail.com>
2017-09-17 22:32:17 -0500

    try canvas

--- src/js/Game.ts
+++ src/js/Game.ts
@@ -1,7 +1,7 @@
 import 'phaser';
 import Boot from './states/Boot'
 
-const game = new Phaser.Game(640, 960, Phaser.AUTO);
+const game = new Phaser.Game(640, 960, Phaser.CANVAS);
 
 //TODO lots of things to revamp if we want to support high dpi :(
 //const game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.CANVAS);
