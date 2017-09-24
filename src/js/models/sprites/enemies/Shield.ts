d7dae530735625c934a7224841834783edd9abaa
lucas <lucas.e.gray@gmail.com>
2017-09-24 17:14:29 -0500

    try singleton emitter

--- src/js/models/sprites/enemies/Shield.ts
+++ src/js/models/sprites/enemies/Shield.ts
@@ -19,25 +19,7 @@ export default class Shield extends PathfindingEnemy {
     kill() {
         super.kill();
 
-        let emitter = this.game.add.emitter(this.x, this.y, 100);
-        emitter.makeParticles('blue-spark');
-        emitter.gravity.setTo(0,0);
-        emitter.maxParticleSpeed.x = 700;
-        emitter.minParticleSpeed.x = -700;
-        emitter.maxParticleSpeed.y = 700;
-        emitter.minParticleSpeed.y = -700;
-        emitter.setScale(.5, .05, .5, .05, 500, Phaser.Easing.Cubic.Out);
-        emitter.start(true, 500, undefined, 10);
-
-        let emitter2 = this.game.add.emitter(this.x, this.y, 100);
-        emitter2.makeParticles('red-spark');
-        emitter2.gravity.setTo(0,0);
-        emitter2.maxParticleSpeed.x = 700;
-        emitter2.minParticleSpeed.x = -700;
-        emitter2.maxParticleSpeed.y = 700;
-        emitter2.minParticleSpeed.y = -700;
-        emitter2.setScale(.5, .05, .5, .05, 500, Phaser.Easing.Cubic.Out);
-        emitter2.start(true, 500, undefined, 10);
+        this.mission.playDeathEmitter(this.x, this.y);
 
         this.destroy();
 
