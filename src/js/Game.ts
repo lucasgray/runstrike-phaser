f55068b4e04adc933ed0c3bfe3c0a24d4caeb0fd
lucas gray <lgray@execvision.io>
2020-09-12 10:54:06 -0500

    update scale

--- src/js/Game.ts
+++ src/js/Game.ts
@@ -1,7 +1,7 @@
 import 'phaser';
 import Boot from './states/Boot'
 
-const game = new Phaser.Game({width: 640, height: 960, renderer: Phaser.CANVAS, scaleMode: Phaser.ScaleManager.EXACT_FIT});
+const game = new Phaser.Game({width: 640, height: 960, renderer: Phaser.WEBGL, scaleMode: Phaser.ScaleManager.SHOW_ALL});
 
 //TODO lots of things to revamp if we want to support high dpi :(
 // const game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.CANVAS);
